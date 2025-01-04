79f6997495ed5a4443490daf91b5e8e2be292024
piirk <baptiste.boucharechas@gmail.com>
2025-01-04 16:24:19 +0100

    implement LoginPage with form handling, NavBar, and Footer components

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NavBar from '../../../common/components/NavBar'
+import Footer from '../../../common/components/Footer'
 
 const LoginPage = () => {
-  return <h1>Login Page (Sample)</h1>
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [rememberMe, setRememberMe] = useState(false)
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+
+    console.log(
+      'Username:',
+      username,
+      'Password:',
+      password,
+      'Remember Me:',
+      rememberMe,
+    )
+  }
+
+  return (
+    <div>
+      <NavBar />
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="input-wrapper">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="input-remember">
+              <input
+                type="checkbox"
+                id="remember-me"
+                checked={rememberMe}
+                onChange={(e) => setRememberMe(e.target.checked)}
+              />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+            {/* <Link to="/profile" className="sign-in-button">
+              Sign In
+            </Link> */}
+            <button className="sign-in-button" type="submit">
+              Sign In
+            </button>
+          </form>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default LoginPage
