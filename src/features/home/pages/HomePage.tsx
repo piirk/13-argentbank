0ee29ca8ee3ea9c671ee75890afe284598e847b2
piirk <baptiste.boucharechas@gmail.com>
2025-01-04 16:05:59 +0100

    implement HomePage layout with NavBar, Hero, Features, and Footer components

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import NavBar from '../../../common/components/NavBar'
+import Hero from '../../../common/components/Hero'
+import Features from '../../../common/components/Features'
+import Footer from '../../../common/components/Footer'
 
 const HomePage = () => {
-  return <h1>Home Page (Sample)</h1>
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Features />
+      <Footer />
+    </div>
+  )
 }
 
 export default HomePage
