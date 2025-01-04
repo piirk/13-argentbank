7603dcf550cd47038fe8b9b87aef3734d4aa37a8
piirk <baptiste.boucharechas@gmail.com>
2025-01-04 16:31:17 +0100

    add ProfilePage with editable username and account sections

--- src/features/profile/pages/ProfilePage.tsx
+++ src/features/profile/pages/ProfilePage.tsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NavBar from '../../../common/components/NavBar'
+import Footer from '../../../common/components/Footer'
+import AccountSection from '../components/AccountSection' // Mise à jour de l'import
 
 const ProfilePage = () => {
-  return <h1>Profile Page (Sample)</h1>
+  const [isEditing, setIsEditing] = useState(false)
+  const [username, setUsername] = useState('Tony Jarvis')
+
+  const handleEditClick = () => {
+    setIsEditing(true)
+  }
+
+  const handleSaveClick = () => {
+    setIsEditing(false)
+    console.log('Nom mis à jour:', username)
+  }
+
+  return (
+    <div>
+      <NavBar />
+
+      <main className="main bg-dark">
+        <div className="header">
+          <h1>
+            Welcome back
+            <br />
+            {username}!
+          </h1>
+          {isEditing ? (
+            <>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="edit-input"
+              />
+              <button onClick={handleSaveClick} className="edit-button">
+                Save Name
+              </button>
+            </>
+          ) : (
+            <button onClick={handleEditClick} className="edit-button">
+              Edit Name
+            </button>
+          )}
+        </div>
+
+        <h2 className="sr-only">Accounts</h2>
+        <AccountSection
+          title="Argent Bank Checking (x8349)"
+          amount="$2,082.79"
+          description="Available Balance"
+        />
+        <AccountSection
+          title="Argent Bank Savings (x6712)"
+          amount="$10,928.42"
+          description="Available Balance"
+        />
+        <AccountSection
+          title="Argent Bank Credit Card (x8349)"
+          amount="$184.30"
+          description="Current Balance"
+        />
+      </main>
+
+      <Footer />
+    </div>
+  )
 }
 
 export default ProfilePage
