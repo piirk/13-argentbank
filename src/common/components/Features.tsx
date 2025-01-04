feedb1e0f4818d9a48e1a6b99758d7ed2c88e6cd
piirk <baptiste.boucharechas@gmail.com>
2025-01-04 16:14:04 +0100

    refactor Features component to use imported icons instead of relative paths

--- src/common/components/Features.tsx
+++ src/common/components/Features.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
+import iconChat from '../../assets/img/icon-chat.png'
+import iconMoney from '../../assets/img/icon-money.png'
+import iconSecurity from '../../assets/img/icon-security.png'
 
 const Features = () => {
   return (
     <section className="features">
       <h2 className="sr-only">Features</h2>
       <div className="feature-item">
-        <img
-          src="./img/icon-chat.png"
-          alt="Chat Icon"
-          className="feature-icon"
-        />
+        <img src={iconChat} alt="Chat Icon" className="feature-icon" />
         <h3 className="feature-item-title">You are our #1 priority</h3>
         <p>
           Need to talk to a representative? You can get in touch through our
@@ -17,20 +16,12 @@ const Features = () => {
         </p>
       </div>
       <div className="feature-item">
-        <img
-          src="./img/icon-money.png"
-          alt="Money Icon"
-          className="feature-icon"
-        />
+        <img src={iconMoney} alt="Money Icon" className="feature-icon" />
         <h3 className="feature-item-title">More savings means higher rates</h3>
         <p>The more you save with us, the higher your interest rate will be!</p>
       </div>
       <div className="feature-item">
-        <img
-          src="./img/icon-security.png"
-          alt="Security Icon"
-          className="feature-icon"
-        />
+        <img src={iconSecurity} alt="Security Icon" className="feature-icon" />
         <h3 className="feature-item-title">Security you can trust</h3>
         <p>
           We use top of the line encryption to make sure your data and money is
