1647fcf3cdd16c99a65e1d8c4971c4d7bc92c2d7
piirk <baptiste.boucharechas@gmail.com>
2025-01-06 17:42:32 +0100

    chore: add TypeScript configuration and update Vite config for module path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@common': path.resolve(__dirname, './src/common'),
+      '@components': path.resolve(__dirname, './src/common/components'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@redux': path.resolve(__dirname, './src/redux'),
+    },
+  },
 })
