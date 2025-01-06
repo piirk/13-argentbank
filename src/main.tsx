afc27f36de1671e9d2c74a60f49ab08f95fadf37
piirk <baptiste.boucharechas@gmail.com>
2025-01-06 16:52:44 +0100

    chore: add Ant Design dependency

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.scss'
+import 'antd/dist/reset.css'
 import App from './app/App'
 
 const rootElement = document.getElementById('root')
