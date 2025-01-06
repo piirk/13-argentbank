4c5998d7ca546c62d84fa5dc82962e2b71653df6
piirk <baptiste.boucharechas@gmail.com>
2025-01-06 19:01:41 +0100

    feat: add axios for API requests and configure axios instance with interceptors

---
+++ src/services/axiosConfig.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { API_URL } from '../config'
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+export default axiosInstance
