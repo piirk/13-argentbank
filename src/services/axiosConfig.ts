5e9d56591d0ce0429715558dcc2a0c7e6c8aa791
piirk <baptiste.boucharechas@gmail.com>
2025-01-07 21:29:05 +0100

    feat: update axios configuration to directly use environment variable for base URL

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL
-
 const axiosInstance = axios.create({
-  baseURL: API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
