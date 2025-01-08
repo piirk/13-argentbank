8f519ee4bff715bfcacf0628773b33fc3410ecd3
piirk <baptiste.boucharechas@gmail.com>
2025-01-08 15:45:12 +0100

    feat: add updateProfile action and moving handling errors in axiosConfig with interceptor

--- src/features/auth/redux/authActions.ts
+++ src/features/auth/redux/authActions.ts
@@ -1,57 +1,28 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
-import axios from 'axios'
+import axiosInstance from '@services/axiosConfig'
 import { setUser } from './authSlice'
-
-const API_URL = import.meta.env.VITE_API_URL
 
 export const login = createAsyncThunk(
   'auth/login',
-  async (
-    userData: { email: string; password: string },
-    { dispatch, rejectWithValue },
-  ) => {
-    try {
-      const loginResponse = await axios.post(API_URL + '/user/login', userData)
-      const { token } = loginResponse.data.body
-      localStorage.setItem('token', token)
+  async (userData: { email: string; password: string }, { dispatch }) => {
+    const loginResponse = await axiosInstance.post('/user/login', userData)
+    const { token } = loginResponse.data.body
+    localStorage.setItem('token', token)
 
-      const profileResponse = await axios.post(
-        API_URL + '/user/profile',
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      )
-      const user = profileResponse.data.body
-      dispatch(setUser(user))
+    const profileResponse = await axiosInstance.post('/user/profile', {})
+    const user = profileResponse.data.body
+    dispatch(setUser(user))
 
-      return { user, token }
-    } catch (error: any) {
-      if (axios.isAxiosError(error)) {
-        if (error.response) {
-          const errorMessages: Record<number, string> = {
-            401: 'Invalid email or password.',
-            400: 'Login failed. Please check your credentials.',
-            403: '403 - Forbidden. You do not have access.',
-            404: '404 - Resource not found.',
-            500: '500 - Internal server error.',
-          }
+    return { user, token }
+  },
+)
 
-          const errorMessage =
-            errorMessages[error.response.status] || 'Login failed'
-          return rejectWithValue(errorMessage)
-        } else if (error.request) {
-          return rejectWithValue('Network error. Unable to reach the server.')
-        } else {
-          console.error('Request setup error:', error.message)
-          return rejectWithValue('An error occurred while making the request.')
-        }
-      } else {
-        console.error('Unhandled error:', error)
-        return rejectWithValue('An unknown error occurred.')
-      }
-    }
+export const updateProfile = createAsyncThunk(
+  'auth/updateProfile',
+  async (userData: { firstName: string; lastName: string }, { dispatch }) => {
+    const response = await axiosInstance.put('/user/profile', userData)
+    const user = response.data.body
+    dispatch(setUser(user))
+    return user
   },
 )
--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -20,4 +20,33 @@ axiosInstance.interceptors.request.use(
   },
 )
 
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        const errorMessages: Record<number, string> = {
+          400: 'Bad request. Please check your input.',
+          401: 'Unauthorized. Please log in again.',
+          403: 'Forbidden. You do not have access.',
+          404: 'Resource not found.',
+          500: 'Internal server error.',
+        }
+
+        const errorMessage =
+          errorMessages[error.response.status] || 'An error occurred'
+        return Promise.reject(errorMessage)
+      } else if (error.request) {
+        return Promise.reject('Network error. Unable to reach the server.')
+      } else {
+        console.error('Request setup error:', error.message)
+        return Promise.reject('An error occurred while making the request.')
+      }
+    } else {
+      console.error('Unhandled error:', error)
+      return Promise.reject('An unknown error occurred.')
+    }
+  },
+)
+
 export default axiosInstance
