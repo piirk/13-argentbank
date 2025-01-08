848170ef36f697a7104ddf8c9ee076362a872646
piirk <baptiste.boucharechas@gmail.com>
2025-01-08 16:47:55 +0100

    feat: remove authMiddleware and update store configuration

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,13 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '@features/auth/redux/authSlice'
-import authMiddleware from '@features/auth/redux/authMiddleware'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authMiddleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
