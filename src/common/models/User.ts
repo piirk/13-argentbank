aed2ca60b8860986cfa366aeeab414565818421c
piirk <baptiste.boucharechas@gmail.com>
2025-01-12 15:52:06 +0100

    refactor: change User class to interface and update property types

--- src/common/models/User.ts
+++ src/common/models/User.ts
@@ -1,24 +1,8 @@
-export class User {
-  id: number
+export interface User {
+  id: string
+  email: string
   firstName: string
   lastName: string
-  email: string
-  createdAt: Date
-  updatedAt: Date
-
-  constructor(
-    id: number,
-    firstName: string,
-    lastName: string,
-    email: string,
-    createdAt: Date,
-    updatedAt: Date,
-  ) {
-    this.id = id
-    this.firstName = firstName
-    this.lastName = lastName
-    this.email = email
-    this.createdAt = createdAt
-    this.updatedAt = updatedAt
-  }
+  createdAt: string
+  updatedAt: string
 }
