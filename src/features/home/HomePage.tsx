2487efe27f3c42d801be30feb5456359303aa931
piirk <baptiste.boucharechas@gmail.com>
2025-01-14 18:22:11 +0100

    feat: refactor Features component with modular styles

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,5 +1,5 @@
 import Hero from '@features/home/components/Hero/Hero'
-import Features from '@features/home/components/Features'
+import Features from '@features/home/components/Features/Features'
 import AppLayout from '@common/components/AppLayout'
 
 const HomePage = () => {
---
+++ src/features/home/components/Features/Features.tsx
@@ -0,0 +1,41 @@
+import iconChat from '@assets/img/icon-chat.png'
+import iconMoney from '@assets/img/icon-money.png'
+import iconSecurity from '@assets/img/icon-security.png'
+import styles from './Features.module.scss'
+
+const Features = () => {
+  return (
+    <section className={styles.features}>
+      <h2 className="sr-only">Features</h2>
+      <div className={styles.featureItem}>
+        <img src={iconChat} alt="Chat Icon" className={styles.featureIcon} />
+        <h3 className={styles.featureItemTitle}>You are our #1 priority</h3>
+        <p>
+          Need to talk to a representative? You can get in touch through our
+          24/7 chat or through a phone call in less than 5 minutes.
+        </p>
+      </div>
+      <div className={styles.featureItem}>
+        <img src={iconMoney} alt="Money Icon" className={styles.featureIcon} />
+        <h3 className={styles.featureItemTitle}>
+          More savings means higher rates
+        </h3>
+        <p>The more you save with us, the higher your interest rate will be!</p>
+      </div>
+      <div className={styles.featureItem}>
+        <img
+          src={iconSecurity}
+          alt="Security Icon"
+          className={styles.featureIcon}
+        />
+        <h3 className={styles.featureItemTitle}>Security you can trust</h3>
+        <p>
+          We use top of the line encryption to make sure your data and money is
+          always safe.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Features
