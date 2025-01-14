213570e3c6b24fbf788e743d1f71567d192ad594
piirk <baptiste.boucharechas@gmail.com>
2025-01-14 18:36:32 +0100

    feat: implement LoginPage with modular styles

--- src/features/login/LoginPage.tsx
+++ src/features/login/LoginPage.tsx
@@ -1,11 +1,12 @@
 import AppLayout from '@common/components/AppLayout'
 import LoginForm from './components/LoginForm'
+import styles from './LoginPage.module.scss'
 
 const LoginPage = () => {
   return (
     <AppLayout>
-      <section className="sign-in-content">
-        <i className="fa fa-user-circle sign-in-icon"></i>
+      <section className={styles.login}>
+        <i className={`fa fa-user-circle ${styles.loginIcon}`}></i>
         <h1>Sign In</h1>
         <LoginForm />
       </section>
