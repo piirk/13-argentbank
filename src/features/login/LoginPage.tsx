9c3269611b502e946497b41e25c3623dab9ed7ec
piirk <baptiste.boucharechas@gmail.com>
2025-01-14 19:14:06 +0100

    feat: update LoginPage to use Ant Design components and improve layout structure

--- src/features/login/LoginPage.tsx
+++ src/features/login/LoginPage.tsx
@@ -1,15 +1,19 @@
+import { Layout, Typography } from 'antd'
 import AppLayout from '@common/components/AppLayout'
 import LoginForm from './components/LoginForm'
 import styles from './LoginPage.module.scss'
 
+const { Content } = Layout
+const { Title } = Typography
+
 const LoginPage = () => {
   return (
     <AppLayout>
-      <section className={styles.login}>
+      <Content className={styles.login}>
         <i className={`fa fa-user-circle ${styles.loginIcon}`}></i>
-        <h1>Sign In</h1>
+        <Title level={1}>Sign In</Title>
         <LoginForm />
-      </section>
+      </Content>
     </AppLayout>
   )
 }
